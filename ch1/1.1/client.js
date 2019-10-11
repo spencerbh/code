77c1679ec03f569f8b6667386c6dc29b6bb3bdbc
Spencer Harris <spencerbh@basinlogix.com>
2019-10-10 18:06:19 -0700

    minor changes, test commit

--- ch1/1.1/client.js
+++ ch1/1.1/client.js
@@ -9,13 +9,13 @@ class Client {
     // TODO
     // create a new Ethereum-identity with EthCrypto.createIdentity()
     // - should create a Javascript object with a privateKey, publicKey and address
-    this.wallet = 'EthCrypto identity object';
+    this.wallet = EthCrypto.createIdentity();
   }
 
   // Creates a keccak256/SHA3 hash of some data
   hash(data) {
     // TODO
-    return 'hash of data';
+    return EthCrypto.hash.keccak256(data);
   }
 
   // Signs a hash of data with the client's private key
