182b4f737b5c6831c6c8faeef9edb03ec9af5735
Spencer Harris <spencerbh@basinlogix.com>
2019-10-13 20:14:28 -0700

    issues remain with tests

--- ch1/1.1/client.js
+++ ch1/1.1/client.js
@@ -6,7 +6,6 @@ class Client {
   // - the public key is like an username or address that people can send stuff to
   // - the private key is like a password or key that allows someone to access the stuff in the account and send transactions/messages from that account
   constructor() {
-    // TODO
     // create a new Ethereum-identity with EthCrypto.createIdentity()
     // - should create a Javascript object with a privateKey, publicKey and address
     this.wallet = EthCrypto.createIdentity();
@@ -14,20 +13,22 @@ class Client {
 
   // Creates a keccak256/SHA3 hash of some data
   hash(data) {
-    // TODO
+    // where is the data item in the demo.js file?
     return EthCrypto.hash.keccak256(data);
   }
 
-  // Signs a hash of data with the client's private key
-  sign(data) {
-    // TODO
-    return 'signed hash';
+  // Signs a hash of data(message?) with the client's private key
+  sign(message) {
+    //
+    const messageHash = this.hash(message);
+    return EthCrypto.sign(this.wallet.privateKey, messageHash);
   }
 
   // Verifies that a messageHash is signed by a certain address
   verify(signature, messageHash, address) {
-    // TODO
-    return 'boolean';
+    // T
+    const signer = EthCrypto.recover(signature, messageHash);
+    return signer === address;
   }
 }
 
